8d481f7739b0c59b09b239ea983bb5e7a008d45f
MDW91 <42375451+MDW91@users.noreply.github.com>
2020-04-15 18:14:25 +0200

    Initial commit

---
+++ MacHandler.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashingOpg
+{
+    class MACHandler
+    {
+
+        private HMAC myMAC;
+
+        public MACHandler(string name)
+        {
+
+
+            if (name.CompareTo("SHA1") == 0)
+            {
+                myMAC = new System.Security.Cryptography.HMACSHA1();
+            }
+            if (name.CompareTo("SHA2") == 0)
+            {
+                myMAC = new System.Security.Cryptography.HMACSHA256();
+            }
+            if (name.CompareTo("MD5") == 0)
+            {
+                myMAC = new System.Security.Cryptography.HMACMD5();
+            }
+
+
+        }
+
+        public bool CheckAuthenticity(byte[] mes, byte[] mac, byte[] key)
+        {
+            myMAC.Key = key;
+            if (CompareByteArrays(myMAC.ComputeHash(mes), mac, myMAC.HashSize / 8) == true)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public byte[] ComputeMAC(byte[] mes, byte[] key)
+        {
+            myMAC.Key = key;
+            return myMAC.ComputeHash(mes);
+        }
+
+        public int MACByteLength()
+        {
+            return myMAC.HashSize / 8;
+        }
+
+        private bool CompareByteArrays(byte[] a, byte[] b, int len)
+        {
+            for (int i = 0; i < len; i++)
+            {
+                if (a[i] != b[i])
+                {
+                    return false;
+                }
+                
+            }
+            return true;
+        }
+
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Security.Cryptography;
+
+namespace HashingOpg
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (cbi.SelectedIndex == 0)
+            {
+                
+                using (SHA1 sha1 = SHA1.Create())
+                {
+                    byte[] hashBytes = sha1.ComputeHash(Encoding.ASCII.GetBytes(plaintext.Text));
+
+                    // Convert the byte array to hexadecimal string
+                    StringBuilder sbhex = new StringBuilder();
+                    for (int i = 0; i < hashBytes.Length; i++)
+                    {
+                        sbhex.Append(hashBytes[i].ToString("X2"));
+                    }
+
+                    hmacTxt.Text = plaintext.Text;
+                    hex.Text = sbhex.ToString();
+                }
+
+            }
+            else if (cbi.SelectedIndex == 1)
+            {
+
+                using (SHA256 sha2 = SHA256.Create())
+                {
+                    byte[] hashBytes = sha2.ComputeHash(Encoding.ASCII.GetBytes(plaintext.Text));
+
+                    // Convert the byte array to hexadecimal string
+                    StringBuilder sbhex = new StringBuilder();
+                    for (int i = 0; i < hashBytes.Length; i++)
+                    {
+                        sbhex.Append(hashBytes[i].ToString("X2"));
+                    }
+
+                    hmacTxt.Text = plaintext.Text;
+                    hex.Text = sbhex.ToString();
+                }
+
+
+            }
+            else if (cbi.SelectedIndex == 2)
+            {
+                string inputText = plaintext.Text;
+                
+                using (MD5 md5 = MD5.Create())
+                {
+                    byte[] hashBytes = md5.ComputeHash(Encoding.ASCII.GetBytes(inputText));
+
+                    // Convert the byte array to hexadecimal string
+                    StringBuilder sbhex = new StringBuilder();
+                    for (int i = 0; i < hashBytes.Length; i++)
+                    {
+                        sbhex.Append(hashBytes[i].ToString("X2"));
+                    }
+
+                    hmacTxt.Text = inputText.ToString();
+                    hex.Text = sbhex.ToString();
+                }
+
+            }
+            else if (cbi.SelectedIndex == 3)
+            {
+
+
+
+
+
+
+
+                plaintext.Text = "HMAC";
+            }
+        }
+
+        private void key_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void hmacTxt_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void TextBox_TextChanged_1(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void ListBoxItem_Selected(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            MACHandler MacHandler = new MACHandler(cbi.Text);
+
+            //MacHandler.ComputeMAC(s,key.Text);
+        }
+
+
+    }
+
+
+}
